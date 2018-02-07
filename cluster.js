fef20f55e004601404c27d03ca309a8d8c6956fa
sekiro <dies.lucky.strike@gmail.com>
2018-02-07 22:54:03 +0900

    cluster.js編集

--- cluster.js
+++ cluster.js
@@ -2,9 +2,11 @@ const cluster = require('cluster');
 const numCPUs = require('os').cpus().length;
 
 if (cluster.isMaster) {
+  console.log(`Master ${process.pid} is running`);
   for (var i = 0; i < numCPUs; i++) {
     cluster.fork();
   }
 } else {
   require("./bin/www");
+  console.log(`Worker ${process.pid} started`);
 }
