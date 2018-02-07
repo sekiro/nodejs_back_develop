42767099ec730d742448cc9e49b5dad030506f7c
sekiro <dies.lucky.strike@gmail.com>
2018-02-07 22:47:20 +0900

    cluster化

---
+++ cluster.js
@@ -0,0 +1,10 @@
+const cluster = require('cluster');
+const numCPUs = require('os').cpus().length;
+
+if (cluster.isMaster) {
+  for (var i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+} else {
+  require("./bin/www");
+}
